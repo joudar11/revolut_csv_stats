9e70072a9823c21738ce85b3d6c52f9679f7c65f
joudar11 <krystofklika@pm.me>
2025-05-21 20:58:52 +0200

    load_csv works and saves csv in operations

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 import csv
 
 
-def load_csv():
+operations = []
+
+
+def load_csv(filename: str):
+    """
+    Loads the Revolut .csv and saves all the lines except for the header in global operations list.
+    """
+    # Date,Ticker,Type,Quantity,Price per share,Total Amount,Currency,FX Rate
+
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader) #skip header
+        for row in reader:
+            operations.append(list(row))
+
+
+def remove_symbols():
+    """
+    Removes the currency symbol for every line in global poerations list.
+    """
+
+
+def fetch_sells() -> float:
     pass
 
 
-def fetch_sells():
+def fetch_buys() -> float:
     pass
 
 
-def fetch_buys():
+def fetch_dividends() -> float:
     pass
 
 
-def fetch_dividends():
-    pass
-
-
-def fetch_fees():
+def fetch_fees() -> float:
     pass
 
 
 def run():
-    pass
+    file = "file.csv"
+    load_csv(file)
+    print(operations)
 
 
 if __name__ == "__main__":
