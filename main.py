162c0195105fb20503942f4fcaece2f356c90cc0
joudar11 <krystofklika@pm.me>
2025-05-21 20:40:15 +0200

    create functions

---
+++ main.py
@@ -0,0 +1,29 @@
+import csv
+
+
+def load_csv():
+    pass
+
+
+def fetch_sells():
+    pass
+
+
+def fetch_buys():
+    pass
+
+
+def fetch_dividends():
+    pass
+
+
+def fetch_fees():
+    pass
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
